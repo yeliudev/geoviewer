f1d8edf8141872db9d2fe390f6dd32c89b059982
Ye Liu <22849092+yeliudev@users.noreply.github.com>
2021-09-19 13:23:43 +0800

    Update api root

--- src/config.js
+++ src/config.js
@@ -4,50 +4,51 @@
 const ACCESS_TOKEN = 'pk.eyJ1IjoieWxpdWRldiIsImEiOiJja2ZldGpkZTcwNDN4MnpvYThsbno3OGd0In0.7OiaH8u5zpRSF7tjly1l7A';
 
 // Server host url
-const API_ROOT = 'https://api.catcatserver.xyz/geoviewer';
+const API_ROOT = 'https://api.catcatdev.com/geoviewer';
 
+// Service endpoints
 const SERVICE = {
-    // User login interface
+    // User login endpoint
     login: {
         url: `${API_ROOT}/login`,
         method: 'POST'
     },
 
-    // User logout interface
+    // User logout endpoint
     logout: {
         url: `${API_ROOT}/logout`,
         method: 'POST'
     },
 
-    // Insert spatial object interface
+    // Insert spatial object endpoint
     insert: {
         url: `${API_ROOT}/insert`,
         method: 'POST'
     },
 
-    // Update spatial object interface
+    // Update spatial object endpoint
     update: {
         url: `${API_ROOT}/update`,
         method: 'POST'
     },
 
-    // Delete spatial object interface
+    // Delete spatial object endpoint
     delete: {
         url: `${API_ROOT}/delete`,
         method: 'POST'
     },
 
-    // Dataset fetching interface
+    // Dataset fetching endpoint
     dataset: {
         url: `${API_ROOT}/dataset`,
         method: 'GET'
     },
 
-    // Spatial query interface
+    // Spatial query endpoint
     search: {
         url: `${API_ROOT}/search`,
         method: 'GET'
     }
-}
+};
 
 export { ACCESS_TOKEN, SERVICE };
